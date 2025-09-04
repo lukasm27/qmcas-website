7370933128ff75a52449cbf92e5eba31beb62193
Lukas Mykhnenko <lmykhnenko@gmail.com>
2025-09-04 14:01:35 +0100

    redirect fix

--- auth.js
+++ auth.js
@@ -4,19 +4,6 @@
   // Check if the URL contains a login token from Netlify Identity.
   // This is the most reliable way to know a user has just logged in.
   if (window.location.hash.includes("access_token")) {
-
-    // First, try to get the page the user was originally trying to visit.
-    // The Netlify widget is supposed to store this for us.
-    const intendedDestination = sessionStorage.getItem("netlify-identity-redirect");
-
-    if (intendedDestination) {
-      // If we found the intended page, go there.
-      // Using .replace() prevents back-button redirect loops.
-      window.location.replace(intendedDestination);
-    } else {
-      // If the widget didn't store the page, this is our fallback.
-      // We will send them to the main members-only page.
-      window.location.replace("/accelerator.html");
-    }
+    location.reload();
   }
 })();
