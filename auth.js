a73f16925029aeb1a00b3c1d6dba3ed7fc61fc92
Lukas Mykhnenko <lmykhnenko@gmail.com>
2025-09-04 13:55:12 +0100

    redirect fix

--- auth.js
+++ auth.js
@@ -1,20 +1,17 @@
 (function() {
-  // This function runs when the Netlify Identity widget is ready.
-  function handleLoginRedirect() {
-    // After a user logs in, Netlify adds a token to the URL hash.
-    // We check for this token to know that a login just happened.
-    if (window.location.hash.includes("access_token")) {
-      // The widget kindly stores the page the user was trying to access.
-      const redirectUrl = sessionStorage.getItem("netlify-identity-redirect");
-      
-      if (redirectUrl) {
-        // If we know where they were going, send them there.
-        // Using replace() prevents back-button redirect loops.
-        window.location.replace(redirectUrl);
-      }
+  // After a user logs in, Netlify Identity adds a hash to the URL
+  // that looks like #access_token=...
+  // We check for this to know that a login just happened.
+  if (window.location.hash.includes("access_token")) {
+    
+    // The widget kindly stores the page the user was trying to access.
+    const redirectUrl = sessionStorage.getItem("netlify-identity-redirect");
+    
+    if (redirectUrl) {
+      // If we know where they were going, send them there.
+      // We use .replace() so this action doesn't get added to the browser history,
+      // which prevents issues with the back button.
+      window.location.replace(redirectUrl);
     }
   }
-
-  // Run our function as soon as the page loads.
-  handleLoginRedirect();
 })();
